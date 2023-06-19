2a3bba6ea6045293432ac8a97cd91d70946de0e4
Sanjoy Roy <sanjoy12k@gmail.com>
2023-06-19 20:39:33 +0530

    First commit

---
+++ Set Matrix Zeros.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+void setZeros(vector<vector<int>> &matrix)
+{
+	// Write your code here.
+	int r = matrix.size();
+	int c = matrix[0].size();
+	vector<int> row(r,-1), col(c,-1);
+
+	for(int i=0; i<r; i++){
+		for(int j = 0; j<c; j++){
+			if(matrix[i][j] == 0){
+				row[i] = 0;
+				col[j] = 0;
+			}
+		}
+	}
+
+	for(int i = 0; i<r; i++){
+		for(int j = 0; j<c; j++){
+			if(row[i] == 0 || col[j] == 0){
+				matrix[i][j] = 0;
+			}
+		}
+	}
+
+
+	
+}
